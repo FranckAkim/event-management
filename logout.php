131a4f7447d23ce6d21de005c32b5e4e34a9a817
FranckAkim <franckdarole@gmail.com>
2026-04-15 04:14:34 -0500

    Delete logout.php

--- logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-// logout.php - Logout and redirect to welcome page
-session_start();
-session_destroy();     // Clear all session data
-header("Location: index.php");   // Redirect to welcome/landing page
-exit;
